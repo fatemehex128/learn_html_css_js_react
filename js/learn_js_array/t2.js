0ef8384a74a75cc301786482e3c8af8e34128b91
amir.shirdeli <amir.shirdeli@zoodfood.com>
2025-11-03 11:45:37 +0330

    add function of array

--- js/learn_js_array/t2.js
+++ js/learn_js_array/t2.js
@@ -1,7 +1,6 @@
-
 // متد های کمکی برای کار با ارایه ها
 // همه این متد ها یک فانکشن میگیرن و در زمانی خاص صداش میزنن و مقداری براش میفرستن برای فهمیدن این که چه مدل تابعی باید برای این متد ها بفرستیم باید سمتندات اش را بخونیم
-let testArray = [1, 2, 3, 4, 5]
+let testArray = [1, 2, 3, 4, 5,19]
 
 
 // متد forEach از ما یک آدرس تابع میگیرد و به اضای هر خونه اریه آدرس تابع ما را صدا میزند و اون خونه ارایه را و کلید اش را برای تابع ما میفرستد
@@ -9,7 +8,7 @@ testArray.forEach((item, index) => {
     console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", item, index)
 })
 // ==
-// اگر خودمان میخواستیم forEach را بسازیم به این شکل میشد
+// باید یک تابع به من بده که سه مقدار بگیره
 let JsForEach = (callbackFn) => {
     for (let key in testArray) {
         callbackFn(testArray[key], key)
@@ -26,6 +25,7 @@ let newTestArray = testArray.map((item, index) => {
     return item * index
 })
 // ==
+// باید یک تابع به من بده که سه مقدار بگیره و یک مقدار جدید واسه اون خونه برگردونه
 let JsMap = (callbackFn) => {
     // یک ارایه جدید ساختیم که مقدار های تغییر یافته را داخلش ذخیره کنیم
     let newArray = []
@@ -51,6 +51,7 @@ let newArrayFiltered = testArray.filter((item) => {
 })
 
 // ==
+// باید یک تابع به من بده که سه مقدار بگیره و یک boolean برگردونه
 let JsFilter = (callbackFn) => {
     // یک ارایه جدید ساختیم که مقدار های تغییر یافته را داخلش ذخیره کنیم
     let newArray = []
@@ -65,26 +66,71 @@ let JsFilter = (callbackFn) => {
     return newArray
 }
 
-// یک تابع دریافت میکند و به آن
-testArray.find((item, index) => {
-    console.log(item, index)
+// یک تابع دریافت میکند این تابع سه ورودی میگیرید مثل بقیه تابع های map filter که خونه اول اش مقداره خونه دوم اش ایندکسه و خونه سوم کل آرایه است . این سه مقدار به تابع ما ارسال میشود و ما با استفاده از این سه مقدار باید بررسی کنیم که شرط ما مطابق ان خونه ارایه هست یا نه اگر بود True و اگر نبود false برگردانیم و زمانی که true برگشته بشه همان خانه را متد find به ما مقدارش را میدهد که به معنی این بود که پیدا شد
+let foundedValue = testArray.find((item, index) => {
+    return item === 5
 })
 
-testArray.some((item, index) => {
-    console.log(item, index)
+// باید یک تابع به من بده که سه مقدار بگیره و یک boolean برگردونه
+let JsFind = (callbackFn) => {
+    for (let key in testArray) {
+        if (callbackFn(testArray[key], key)) {
+            return testArray[key]
+        }
+    }
+}
+// یک تابع میگیرد و سه تا خونه میفرستد و یک boolean میخواد
+// اگر حتی یکی از خونه ها توسط تابع ما true برگشت داده بشه مقدار برگشتی این متد true هست
+let haveOneOf = testArray.some((item) => {
+    return item > 18
 })
 
-testArray.every((item, index) => {
-    console.log(item, index)
+let JsSome = (callbackFn) => {
+    for (let key in testArray) {
+        if (callbackFn(testArray[key], key)) {
+            return true
+        }
+    }
+    return false
+}
+
+// یک تابع میگیرد و سه تا خونه میفرستد و یک boolean میخواد
+// کارش این هست که ببیند ایا همه خونه های ارایه با شرط ما صدق میکند یا نه
+// اگر حتی یک خونه از ارایه با شرط ما false بشه مقدار برگشتی این متد False
+let checkedStatus = testArray.every((item) => {
+    if (item > 0) {
+        return true
+    } else {
+        return false
+    }
 })
 
-
-testArray.reduce((acc, item, index) => {
-    console.log(item, index)
-})
+let JsEvery = (callbackFn) => {
+    for (let key in testArray) {
+        if (!callbackFn(testArray[key], key)) {
+            return false
+        }
+    }
+    return true
+}
+// اگر بهش تابع ندیم به صورت معمولی مرتب میکند از کوچیک به بزرگ
+// این حالت زمانی کار میکند که ارایه ما قابل مرتب سازی باشد یعنی فقط عدد باشه یا فقط استرینگ باشه
+myArr4.sort()
+myArr4.reverse()
 
 myArr4.sort((a, b) => {
-    console.log(a, b)
+    return a - b
 })
 
+let JsSort = (callbackFn) => {
+    for (let i = 0; i < testArray.length; i++) {
+        for (let j = i + 1; j < testArray.length; j++) {
+            if (callbackFn(testArray[i], testArray[j]) > 0) {
+                let temp = testArray[i]
+                testArray[i] = testArray[j]
+                testArray[j] = temp
+            }
+        }
+    }
 
+}
