a064571d1b7c588fc4fc1d9370e5f4fdb00dab1a
amir.shirdeli <amir.shirdeli@zoodfood.com>
2025-11-23 08:24:10 +0330

    add new component

--- react/learn_react_3/src/components/CreateArticle.jsx
+++ react/learn_react_3/src/components/CreateArticle.jsx
@@ -12,6 +12,7 @@ export default () => {
     const [gender, setGender] = useState("")
 
 
+    const [taskList, setTaskList] = useState([])
     // دو راه برای ست کردن مقدار جدید دارین
     // مستقیم خودمان داده را ست کنیم
     // setRangePrice(rangePrice + 100)
@@ -24,55 +25,75 @@ export default () => {
     // یک تابع میگیرند و هر وقت اتقاف رخ داد صداش میزنن
     // کال بک فانکشن
     return (<div>
-            <div>
-                <input value={title} onChange={(e) => {
-                    setTitle(e.target.value)
-                }} type="text" placeholder={"Title"}/>
-                <div><textarea value={content} onChange={(e) => {
-                    setContent(e.target.value)
-                }} placeholder={"Content"}></textarea></div>
-                <div>رنگ<input value={color} onChange={(e) => {
-                    setColor(e.target.value)
-                }} type="color"/></div>
-                <div>تاریخ حذف<input value={date} onChange={(e) => {
-                    setDate(e.target.value)
-                }} type="date"/></div>
-                <div>مخصوص چه رنج افرادی باشد<input value={rangePrice} onChange={(e) => {
-                    setRangePrice(e.target.value)
-                }} type="range"/></div>
-                <select value={deletedAt} onChange={(e) => {
-                    setDeletedAt(e.target.value)
-                }}>
-                    <option value="fast">انتشار فوری</option>
-                    <option value="tomorrow">فردا</option>
-                    <option value={"nexTomorrow"}>پس فردا</option>
-                </select>
-                <div>ارسال به تلگرام : <input checked={isTelegram} onChange={(e) => {
-                    setIsTelegram(e.target.checked)
-                }} type="checkbox"/></div>
-                <div>ارسال به اینستاگرام : <input checked={isInstagram} onChange={(e) => {
-                    setIsInstagram(e.target.checked)
-                }} type="checkbox"/></div>
-                مخصوص چه افرادی هست؟
-                <div>زن<input value={"zan"} checked={gender==="zan"} onChange={(e) => {
-                    setGender(e.target.value)
-                }} name="gender" type="radio"/></div>
-                <div>مرد<input value={"mard"} checked={gender==="mard"} onChange={(e) => {
-                    setGender(e.target.value)
-                }} name="gender" type="radio"/></div>
+        <div>
+            <input value={title} onChange={(e) => {
+                setTitle(e.target.value)
+            }} type="text" placeholder={"Title"}/>
+            <div><textarea value={content} onChange={(e) => {
+                setContent(e.target.value)
+            }} placeholder={"Content"}></textarea></div>
+            <div>رنگ<input value={color} onChange={(e) => {
+                setColor(e.target.value)
+            }} type="color"/></div>
+            <div>تاریخ حذف<input value={date} onChange={(e) => {
+                setDate(e.target.value)
+            }} type="date"/></div>
+            <div>مخصوص چه رنج افرادی باشد<input value={rangePrice} onChange={(e) => {
+                setRangePrice(e.target.value)
+            }} type="range"/></div>
+            <select value={deletedAt} onChange={(e) => {
+                setDeletedAt(e.target.value)
+            }}>
+                <option value="fast">انتشار فوری</option>
+                <option value="tomorrow">فردا</option>
+                <option value={"nexTomorrow"}>پس فردا</option>
+            </select>
+            <div>ارسال به تلگرام : <input checked={isTelegram} onChange={(e) => {
+                setIsTelegram(e.target.checked)
+            }} type="checkbox"/></div>
+            <div>ارسال به اینستاگرام : <input checked={isInstagram} onChange={(e) => {
+                setIsInstagram(e.target.checked)
+            }} type="checkbox"/></div>
+            مخصوص چه افرادی هست؟
+            <div>زن<input value={"zan"} checked={gender === "zan"} onChange={(e) => {
+                setGender(e.target.value)
+            }} name="gender" type="radio"/></div>
+            <div>مرد<input value={"mard"} checked={gender === "mard"} onChange={(e) => {
+                setGender(e.target.value)
+            }} name="gender" type="radio"/></div>
 
-                <button>ارسال</button>
-                <hr/>
-                <div>title : {title}</div>
-                <div>content : {content}</div>
-                <div>date : {date}</div>
-                <div>color : {color}</div>
-                <div>deletedAt : {deletedAt}</div>
-                <div>rangePrice : {rangePrice}</div>
-                <div>isTelegram : {isTelegram?"true":"false"}</div>
-                <div>isInstagram : {isInstagram?"true":"false"}</div>
-                <div>gender : {gender}</div>
-            </div>
+            <button onClick={() => {
+                let finalData = {
+                    title: title,
+                    content: content,
+                    date: date,
+                    color: color,
+                    deletedAt: deletedAt,
+                    rangePrice: rangePrice,
+                    isTelegram: isTelegram,
+                    isInstagram: isInstagram,
+                    gender: gender,
+                }
+                setTaskList([...taskList, finalData])
+                // taskList.push(finalData)
+            }}>ارسال
+            </button>
+            <hr/>
+            <div>title : {title}</div>
+            <div>content : {content}</div>
+            <div>date : {date}</div>
+            <div>color : {color}</div>
+            <div>deletedAt : {deletedAt}</div>
+            <div>rangePrice : {rangePrice}</div>
+            <div>isTelegram : {isTelegram ? "true" : "false"}</div>
+            <div>isInstagram : {isInstagram ? "true" : "false"}</div>
+            <div>gender : {gender}</div>
+        </div>
 
-        </div>)
+
+        <div>
+            {taskList.map(x=><div>{x.title} - {x.color} - {x.rangePrice} - {x.gender} - {x.isTelegram} </div>)}
+        </div>
+
+    </div>)
 }
